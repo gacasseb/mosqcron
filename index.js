4ef6dae36df083836c05c10a675450d15e0322f2
Gabriel Casseb <gabriel.casseb@praxent.com>
2023-06-18 17:40:55 -0300

    added debug + try catch

--- index.js
+++ index.js
@@ -4,36 +4,43 @@ require("dotenv").config();
 
 const LOG_PATH = process.env.LOG_PATH;
 
-fs.readFile(LOG_PATH, "utf8", function (err, data) {
-  if (err) throw err;
-  const lines = data.split("\n");
-  const date = lines[0];
-  if (date === "DONE") {
-    console.log("File already processed");
-    return;
-  }
+try {
+  console.log("Running cronjob to send classifications to mosquitto");
+  fs.readFile(LOG_PATH, "utf8", function (err, data) {
+    if (err) throw err;
+    const lines = data.split("\n");
+    const date = lines[0];
+    if (date === "DONE") {
+      console.log("File already processed");
+      return;
+    }
 
-  const client = mqtt.connect("mqtt://44.211.255.54");
+    const client = mqtt.connect("mqtt://44.211.255.54");
 
-  client.on("connect", () => {
-    lines.shift();
+    client.on("connect", () => {
+      console.log("Connected to the broker");
+      lines.shift();
 
-    const classifications = lines.map((line, index) => {
-      const classification = line.split("-")[1];
-      return {
-        index,
-        classification,
-      };
+      const classifications = lines.map((line, index) => {
+        const classification = line.split("-")[1];
+        return {
+          index,
+          classification,
+        };
+      });
+
+      client.publish("mosqlink", JSON.stringify(classifications));
+      console.log("Sent classifications to mosquitto");
+
+      fs.writeFile(LOG_PATH, "DONE\n" + data, (err) => {
+        if (err) throw err;
+
+        console.log(`Saved`);
+      });
+
+      client.end();
     });
-
-    client.publish("mosqlink", JSON.stringify(classifications));
-
-    fs.writeFile(LOG_PATH, "DONE\n" + data, (err) => {
-      if (err) throw err;
-
-      console.log(`Saved`);
-    });
-
-    client.end();
   });
-});
+} catch (error) {
+  console.error(error)
+}
